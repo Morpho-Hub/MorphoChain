2bda9a281d279c48449ea19a69e41ff5cd7fc614
dd0292 <david.chaves0603@gmail.com>
2025-10-29 13:09:11 -0600

    feature(backend): finca endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import userRouter from './routes/users';
+import fincaRoutes from './routes/fincas';
+import cropRoutes from './routes/crops';
+import tokenRoutes from './routes/tokens';
 
 const router = express.Router();
 
 router.use('/users', userRouter);
+router.use('/fincas', fincaRoutes);
+router.use('/crops', cropRoutes);
+router.use('/tokens', tokenRoutes);
 
 export default router;
